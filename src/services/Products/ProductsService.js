4e8c0b4dce93d3a9d35ba4692a20bd6dfd55edf4
Eduardo Marques <edu.temarques@gmail.com>
2019-10-27 19:14:36 -0300

    feat: Add product service

---
+++ src/services/Products/ProductsService.js
@@ -0,0 +1,13 @@
+import { APIBaseURL } from "../../configs/APIBaseUrl"
+
+export const getAll = () => {
+  return fetch(`${APIBaseURL}/products`)
+    .then(response => response.json())
+    .then(body => body)
+}
+
+export const getAllByFilter = filters => {
+  return fetch(`${APIBaseURL}/products/?${filters}`)
+    .then(response => response.json())
+    .then(body => body)
+}
